4f7ad90e08a681b889a4c927ff2da901d9ffa97c
Dh1910 <dhyanimarvaniya@gamil.com>
2024-03-18 12:26:01 +0530

    add operator code

---
+++ a1.java
@@ -0,0 +1,17 @@
+package practice-java;
+import java.util.*;
+class a1
+{
+	public static void main(String[] args)
+	{
+		Scanner s1=new Scanner(System.in);
+		System.out.println("Enter number 1 :");
+		float n1=s1.nextFloat();
+		System.out.println("Enter number 2 :");
+		float n2=s1.nextFloat();
+		
+		System.out.println("Addition :"+(n1+n2) + " Division :"+(n1/n2) + " Multiplication :"+(n1*n2) + " Subtraction :"+(n1-n2) + " Modeulo :"+(n1%n2));
+		
+	}
+}
+
