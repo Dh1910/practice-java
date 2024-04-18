fb45d4a096485409396bf98cd4b7b4bf002e1706
Dh1910 <dhyanimarvaniya@gamil.com>
2024-04-18 22:09:16 +0530

    add code to print simple string

---
+++ d1.java
@@ -0,0 +1,11 @@
+class d1
+{
+	public static void main(String args[])
+	{
+		String name=args[0];
+		String mname=args[1];
+		String surname=args[2];
+		
+		System.out.println(name+" "+mname+" "+surname);
+	}
+}
