7cc5275fe99e53b9df46c861eba054dda299ceb4
Dh1910 <dhyanimarvaniya@gamil.com>
2024-05-07 11:39:01 +0530

    add code to maximum numbers for if else loop

---
+++ max.java
@@ -0,0 +1,14 @@
+class max 
+{
+	public static void main(String args[])
+	{
+		int a=34,b=12,c=7;
+		if(a>b && a>c)
+		System.out.println("a is max");
+		else if(b>c)
+		System.out.println("b is max");
+		else
+		System.out.println("c is max");
+
+	}
+}
