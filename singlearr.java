e3869ad17570bc13f57f5f8bddbd88551008fbdf
Dh1910 <dhyanimarvaniya@gamil.com>
2024-05-18 07:43:32 +0530

    add code to one dimensional array

---
+++ singlearr.java
@@ -0,0 +1,11 @@
+class singlearr
+{
+	public static void main(String args[])
+	{
+		int arr[]={2,8,96,12};
+		for(int i=0;i<=arr.length;i++)
+		{
+			System.out.println("Enter String :"+arr[i]);
+		}
+	}
+}
