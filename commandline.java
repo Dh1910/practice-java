945481bc19d985aadcb1e2709579885ad66108b8
Dh1910 <dhyanimarvaniya@gamil.com>
2024-05-19 19:49:38 +0530

    add code to commandline argument

---
+++ commandline.java
@@ -0,0 +1,13 @@
+class commandline
+{
+	public static void main(String args[])
+	{
+		String maths=args[0];
+		String physics=args[1];
+		String chemistry=args[2];
+		String biology=args[3];
+		
+		System.out.println(+maths + " " + physics + " " + chemistry + " " +biology);
+		
+	}
+}
